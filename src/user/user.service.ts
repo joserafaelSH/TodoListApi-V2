a6761c42a6021c403e9790f42a506d227a590b5c
joserafaelSH <jrhermoso18@gmail.com>
2023-01-08 21:26:57 -0300

    refactor: alteracao do tipo do ID

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,18 +12,18 @@ export class UserService {
         return this.userRepository.findAll();
     }
 
-    findOne(id: number) {
+    findOne(id: string) {
         return this.userRepository.findOne(id);
     }
 
-    update(id: number, updateUserDto: UpdateUserDto) {
+    update(id: string, updateUserDto: UpdateUserDto) {
         return this.userRepository.update(
             id,
             updateUserDto,
         );
     }
 
-    remove(id: number) {
+    remove(id: string) {
         return this.userRepository.remove(id);
     }
 }
