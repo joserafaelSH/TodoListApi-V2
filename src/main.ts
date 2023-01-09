8bd9f5a8158a304a2360ba81011ee129ddb46d11
joserafaelSH <jrhermoso18@gmail.com>
2023-01-08 21:21:38 -0300

    feat: add globalpipe validation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,6 +6,13 @@ import { setup_swagger } from './config/setup-swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+        }),
+    );
+
     setup_swagger(app);
 
     const CONFIG_SERVICE = app.get(ConfigService);
