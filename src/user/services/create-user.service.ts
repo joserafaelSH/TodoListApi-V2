a5604c41b5aa4980b1490e4c8941585804d1d30d
joserafaelSH <jrhermoso18@gmail.com>
2023-01-08 21:26:40 -0300

    feat: createUserService

--- src/user/services/create-user.service.ts
+++ src/user/services/create-user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto, SerializedUserDto } from '../dto';
 import { UserRepository } from '../user.repository';
 
 @Injectable()
@@ -6,6 +7,10 @@ export class CreateUserService {
     constructor(
         private readonly userRepository: UserRepository,
     ) {}
+
+    create(user: CreateUserDto) {
+        return this.userRepository.users.push(user);
+    }
 
     //cria o usuario no bd aqui
 }
